261ef298bc4ff3dd70a5b16d46a76290aa3c6a29
Austin <austin@datarats.com>
2013-09-12 16:34:46 -0700

    setting the facts

---
+++ README.md
@@ -0,0 +1,15 @@
+CatFacts
+========
+
+Prank your friends by sending them cat facts through google voice sms. The script will run infinity, sending cat facts every 10 minutes.
+
+## Usage
+* ruby catfacts.rb example@gmail.com 8881114444 Targetname
+* Eample: ruby catfacts.rb steveperry@gmail.com 5554441212 Steve
+
+After providing the script with the correct args, you will be prompted for your google password.
+
+
+
+
+
---
+++ catfacts.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'google_text'
+require 'highline/import'
+require 'timeout'
+
+class CatFacts
+
+  def initialize
+    username = ARGV[0]
+    password = ask("What is your Google Password?\n") { |q| q.echo = false }
+    GoogleText.configure {|config| config.email, config.password = username, password}
+  end
+
+  def send_sms
+    number = ARGV[1]
+    name = ARGV[2]
+    greeting = GoogleText::Message.new(:text => "Hello #{name}, thanks for signing up to CatFacts! You will receive a free cat fact every 10 secs! Text 'Cat' to stop receiving CatFacts!", :to => number)
+    greeting.send
+
+    1.upto(Float::INFINITY) do |i|
+      file = File.read('catfacts').split(/\n/)
+      fact = file[Random.rand(file.size)][0..-2]
+      message = GoogleText::Message.new(:text => fact, :to => number)
+      message.send
+      puts "Sent This Cat Fact: #{fact}"
+      sleep(600)
+    end
+  end
+
+end
+
+cf = CatFacts.new
+cf.send_sms
